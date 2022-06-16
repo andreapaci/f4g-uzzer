beb2e41d1f3e9ee6bf7151185b26c4f21a261faa
andreapaci <andrea.paci1998@gmail.com>
2022-06-16 16:55:57 +0200

    Cleaned output of the program

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Starting fuzzer"
-sudo -E /home/andrea/Desktop/Fuzzer/f4g-uzzer/venv/bin/python3.6 /home/andrea/Desktop/Fuzzer/f4g-uzzer/main.py
+sudo -E /home/andrea/Desktop/Work/f4g/venv/bin/python3.6 /home/andrea/Desktop/Work/f4g/main.py
